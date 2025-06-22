a51e374fc224839c5e25b91cc33e85a4f6c76ef4
Apolo <apolovictorj.c@gmail.com>
2025-06-22 15:34:27 -0300

    Correção na main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,6 +1,8 @@
 // src/app/Main.java
 package app;
 
+import jogar.Jogo;
+
 public class Main {
     public static void main(String[] args) {
         // basta instanciar e chamar o método da sua fachada
